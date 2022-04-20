eb3d856663527de4dab3a1bd7481635893762f4f
Zahro Nurrobby Ashari <zahronurrobbya@gmail.com>
2022-04-20 20:26:45 +0700

    setup firebasecore (android)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pwpb1/app/modules/global/splash/splash_binding.dart';
@@ -6,7 +7,9 @@ import 'package:pwpb1/app/modules/global/splash/splash_view.dart';
 import 'core/theme/app_theme.dart';
 import 'routes/pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: Routes.SPLASH,
